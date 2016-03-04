e8bf52cda8b32d45187ca9a2a52d72383c8330f5
Gavin Williams <fatmcgav@gmail.com>
2016-03-04 18:51:50 +0000

    Initial commit

---
+++ resource_provisioner.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform/helper/config"
+	"github.com/hashicorp/terraform/terraform"
+
+	clc "github.com/CenturyLinkCloud/clc-sdk"
+	"github.com/CenturyLinkCloud/clc-sdk/api"
+	"github.com/CenturyLinkCloud/clc-sdk/server"
+	"github.com/CenturyLinkCloud/clc-sdk/status"
+
+	"github.com/mitchellh/mapstructure"
+
+	"github.com/kr/pretty"
+)
+
+type Provisioner struct {
+	Username          string            `mapstructure:"username"`
+	Password          string            `mapstructure:"password"`
+	Account           string            `mapstructure:"account"`
+	Package           string            `mapstructure:"package"`
+	PackageParameters map[string]string `mapstructure:"parameters"`
+}
+
+type ResourceProvisioner struct{}
+
+func (r *ResourceProvisioner) Apply(
+	o terraform.UIOutput,
+	s *terraform.InstanceState,
+	c *terraform.ResourceConfig) error {
+
+	log.Printf("Got to Apply for clc_exec provisioner")
+
+	log.Printf("InstanceState looks like:")
+	// fmt.Printf("%+v\n", s)
+	// fmt.Printf("InstanceState: %# v", pretty.Formatter(s))
+	pretty.Log(s)
+
+	// Decode the raw config for this provisioner
+	p, err := r.decodeConfig(c)
+	if err != nil {
+		return err
+	}
+
+	log.Print("Provisioner looks like:")
+	pretty.Log(p)
+
+	log.Printf("Username = %s, Password = %s, Alias = %s", p.Username, p.Password, p.Account)
+	log.Printf("Attempting to execute package %s", p.Package)
+	log.Print("Package Parameters looks like:")
+	pretty.Log(p.PackageParameters)
+
+	server_id := s.ID
+	log.Printf("Server name = %s", server_id)
+	o.Output(fmt.Sprintf("Executing package '%s' on server '%s'", p.Package, server_id))
+
+	// Create a CLC config
+	config, config_err := api.NewConfig(p.Username, p.Password, p.Account, "")
+	if config_err != nil {
+		return fmt.Errorf("Failed to create CLC config with provided details: %v", config_err)
+	}
+
+	// Create a new CLC Client
+	client := clc.New(config)
+
+	// Make sure we can authentication
+	auth_err := client.Authenticate()
+	if auth_err != nil {
+		return fmt.Errorf("Failed authenticated with provided credentials: %v", err)
+	}
+
+	// Create the pkg structure
+	package_exec_spec := server.Package{
+		ID:     p.Package,
+		Params: p.PackageParameters,
+	}
+
+	// Execute the package
+	pkg_resp, pkg_err := client.Server.ExecutePackage(package_exec_spec, server_id)
+	if pkg_err != nil || !pkg_resp.IsQueued {
+		return fmt.Errorf("Failed executing package: %v", pkg_err)
+	}
+
+	// Check status
+	ok, st := pkg_resp.GetStatusID()
+	if !ok {
+		return fmt.Errorf("Failed extracting status to poll on %v: %v", pkg_resp, pkg_err)
+	}
+	err = waitStatus(client, st)
+	if err != nil {
+		return err
+	}
+
+	o.Output(fmt.Sprintf("Package %s successfully executed on %s", p.Package, server_id))
+
+	return nil
+}
+
+func (r *ResourceProvisioner) Validate(c *terraform.ResourceConfig) (ws []string, es []error) {
+	v := &config.Validator{
+		Required: []string{
+			"username", "password", "account", "package",
+		},
+		Optional: []string{
+			"parameters.*",
+		},
+	}
+	return v.Validate(c)
+}
+
+func (r *ResourceProvisioner) decodeConfig(c *terraform.ResourceConfig) (*Provisioner, error) {
+	log.Printf("Got to decodeConfig for clc_exec")
+
+	log.Print("ResourceConfig looks like:")
+	pretty.Log(c)
+
+	p := new(Provisioner)
+
+	decConf := &mapstructure.DecoderConfig{
+		ErrorUnused:      true,
+		WeaklyTypedInput: true,
+		Result:           p,
+	}
+
+	dec, err := mapstructure.NewDecoder(decConf)
+	if err != nil {
+		return nil, err
+	}
+
+	m := make(map[string]interface{})
+	for k, v := range c.Raw {
+		m[k] = v
+	}
+
+	for k, v := range c.Config {
+		m[k] = v
+	}
+
+	if err := dec.Decode(m); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+
+}
+
+// package utility functions
+
+func waitStatus(client *clc.Client, id string) error {
+	// block until queue is processed and server is up
+	poll := make(chan *status.Response, 1)
+	err := client.Status.Poll(id, poll)
+	if err != nil {
+		return nil
+	}
+	status := <-poll
+	log.Printf("[DEBUG] status %v", status)
+	if status.Failed() {
+		return fmt.Errorf("unsuccessful job %v failed with status: %v", id, status.Status)
+	}
+	return nil
+}
