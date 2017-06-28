3226280ff8add929ce3c4923e6c5ae2be1c72e03
Gavin Williams <fatmcgav@gmail.com>
2017-06-28 14:35:27 +0100

    Update plugin to use  helper to do heavy lifting

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/hashicorp/terraform/terraform"
 )
 
-func ResourceProvisionerBuilder() terraform.ResourceProvisioner {
-	return &ResourceProvisioner{}
-}
-
 func main() {
-	serveOpts := &plugin.ServeOpts{
-		ProvisionerFunc: ResourceProvisionerBuilder,
-	}
-
-	plugin.Serve(serveOpts)
+	plugin.Serve(&plugin.ServeOpts{
+		ProvisionerFunc: Provisioner,
+	})
 }
--- resource_provisioner.go
+++ resource_provisioner.go
@@ -1,67 +1,87 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log"
-	"os"
-
-	"github.com/hashicorp/terraform/helper/config"
-	"github.com/hashicorp/terraform/terraform"
 
 	clc "github.com/CenturyLinkCloud/clc-sdk"
 	"github.com/CenturyLinkCloud/clc-sdk/api"
 	"github.com/CenturyLinkCloud/clc-sdk/server"
 	"github.com/CenturyLinkCloud/clc-sdk/status"
-
-	"github.com/mitchellh/mapstructure"
-
-	"github.com/kr/pretty"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/mitchellh/go-linereader"
 )
 
-type Provisioner struct {
-	Username          string            `mapstructure:"username"`
-	Password          string            `mapstructure:"password"`
-	Account           string            `mapstructure:"account"`
-	Package           string            `mapstructure:"package"`
-	PackageParameters map[string]string `mapstructure:"parameters"`
+func Provisioner() terraform.ResourceProvisioner {
+	return &schema.Provisioner{
+		Schema: map[string]*schema.Schema{
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CLC_USERNAME", nil),
+			},
+
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CLC_PASSWORD", nil),
+			},
+
+			"account": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CLC_ACCOUNT", nil),
+			},
+
+			"package": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"parameters": &schema.Schema{
+				Type:     schema.TypeMap,
+				Required: true,
+			},
+		},
+
+		ApplyFunc: applyFn,
+	}
 }
 
-type ResourceProvisioner struct{}
+// Apply executes the CLC Provisioer
+func applyFn(ctx context.Context) error {
+	data := ctx.Value(schema.ProvConfigDataKey).(*schema.ResourceData)
+	instanceState := ctx.Value(schema.ProvRawStateKey).(*terraform.InstanceState)
+	o := ctx.Value(schema.ProvOutputKey).(terraform.UIOutput)
 
-func (r *ResourceProvisioner) Apply(
-	o terraform.UIOutput,
-	s *terraform.InstanceState,
-	c *terraform.ResourceConfig) error {
+	o.Output("Executing applyFn")
 
-	log.Print("Got to Apply for clc_exec provisioner")
+	// Get required config items
+	username := data.Get("username").(string)
+	password := data.Get("password").(string)
+	account := data.Get("account").(string)
+	packageUUID := data.Get("package").(string)
+	o.Output(fmt.Sprintf("Username = %s, Password = %s, PackageUUID = %s", username, password, packageUUID))
 
-	log.Print("InstanceState looks like:")
-	// fmt.Printf("%+v\n", s)
-	// fmt.Printf("InstanceState: %# v", pretty.Formatter(s))
-	pretty.Log(s)
+	// Need to know serverID to use later
+	serverID := instanceState.ID
+	o.Output(fmt.Sprintf("serverID = %s", serverID))
 
-	var err error
+	// Get any package parameters
+	parametersRaw := data.Get("parameters").(map[string]interface{})
 
-	// Decode the raw config for this provisioner
-	p, err := r.decodeConfig(c)
-	if err != nil {
-		return err
+	// Process parametersRaw
+	parameters := make(map[string]string)
+	for k, v := range parametersRaw {
+		parameters[k] = v.(string)
 	}
-
-	log.Print("Provisioner looks like:")
-	pretty.Log(p)
-
-	log.Printf("Username = %s, Password = %s, Alias = %s", p.Username, p.Password, p.Account)
-	log.Printf("Attempting to execute package %s", p.Package)
-	log.Print("Package Parameters looks like:")
-	pretty.Log(p.PackageParameters)
-
-	server_id := s.ID
-	log.Printf("Server name = %s", server_id)
-	o.Output(fmt.Sprintf("Executing package '%s' on server '%s'", p.Package, server_id))
+	o.Output(fmt.Sprintf("ParametersRaw = %+v, Processed = %+v", parametersRaw, parameters))
 
 	// Create a CLC config
-	config, err := api.NewConfig(p.Username, p.Password, p.Account, "")
+	config, err := api.NewConfig(username, password, account, "")
 	if err != nil {
 		return fmt.Errorf("Failed to create CLC config with provided details: %v", err)
 	}
@@ -77,13 +97,13 @@ func (r *ResourceProvisioner) Apply(
 
 	// Create the pkg structure
 	package_exec_spec := server.Package{
-		ID:     p.Package,
-		Params: p.PackageParameters,
+		ID:     packageUUID,
+		Params: parameters,
 	}
 
 	// Execute the package
 	// TODO: Is this a bit hacky just picking the first array entry?
-	resp, err := client.Server.ExecutePackage(package_exec_spec, server_id)
+	resp, err := client.Server.ExecutePackage(package_exec_spec, serverID)
 	if err != nil || !resp[0].IsQueued {
 		return fmt.Errorf("Failed executing package: %v", err)
 	}
@@ -99,83 +119,18 @@ func (r *ResourceProvisioner) Apply(
 		return err
 	}
 
-	o.Output(fmt.Sprintf("Package %s successfully executed on %s", p.Package, server_id))
+	// Got here, so it must have worked! :)
+	o.Output(fmt.Sprintf("Package %s successfully executed on %s", packageUUID, serverID))
 
 	return nil
 }
 
-func (r *ResourceProvisioner) Validate(c *terraform.ResourceConfig) (ws []string, es []error) {
-	log.Print("Got to Validate for clc_exec")
-	log.Print("Initial ResourceConfig looks like:")
-	pretty.Log(c)
-
-	v := &config.Validator{
-		Required: []string{
-			"username", "password", "account", "package",
-		},
-		Optional: []string{
-			"parameters.*",
-		},
+func copyOutput(o terraform.UIOutput, r io.Reader, doneCh chan<- struct{}) {
+	defer close(doneCh)
+	lr := linereader.New(r)
+	for line := range lr.Ch {
+		o.Output(line)
 	}
-
-	wrn, err := v.Validate(c)
-
-	if len(wrn) > 0 || len(err) > 0 {
-		log.Print("Got some errors returned from Validate")
-		pretty.Log(wrn)
-		pretty.Log(err)
-
-		// Populate c from env variables
-		getEnv(c)
-
-		// Revalidate
-		log.Print("Revalidating...")
-		return v.Validate(c)
-	} else {
-		// No issues
-		log.Print("No issues with ResourceConfig, returning...")
-		return
-	}
-
-}
-
-func (r *ResourceProvisioner) decodeConfig(c *terraform.ResourceConfig) (*Provisioner, error) {
-	log.Print("Got to decodeConfig for clc_exec")
-
-	p := new(Provisioner)
-
-	decConf := &mapstructure.DecoderConfig{
-		ErrorUnused:      true,
-		WeaklyTypedInput: true,
-		Result:           p,
-	}
-
-	dec, err := mapstructure.NewDecoder(decConf)
-	if err != nil {
-		return nil, err
-	}
-
-	// Populate c from env variables
-	getEnv(c)
-
-	log.Print("ResourceConfig looks like:")
-	pretty.Log(c)
-
-	m := make(map[string]interface{})
-	for k, v := range c.Raw {
-		m[k] = v
-	}
-
-	for k, v := range c.Config {
-		m[k] = v
-	}
-
-	if err := dec.Decode(m); err != nil {
-		return nil, err
-	}
-
-	return p, nil
-
 }
 
 // package utility functions
@@ -193,25 +148,4 @@ func waitStatus(client *clc.Client, id string) error {
 		return fmt.Errorf("unsuccessful job %v failed with status: %v", id, status.Status)
 	}
 	return nil
-}
-
-func getEnv(c *terraform.ResourceConfig) {
-	// Need to grab Env variables then...
-	envVars := map[string]string{
-		"CLC_USERNAME": "username",
-		"CLC_PASSWORD": "password",
-		"CLC_ACCOUNT":  "account",
-	}
-
-	for env, config := range envVars {
-		if v := os.Getenv(env); v != "" {
-			log.Printf("Got a value for env '%s': %s", env, v)
-			// Set the config value in ResourceConfig
-			c.Raw[config] = v
-			c.Config[config] = v
-		}
-	}
-	log.Print("Tweaked ResourceConfig looks like:")
-	pretty.Log(c)
-
 }
